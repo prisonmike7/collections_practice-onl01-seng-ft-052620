4c17c287dbe8c3af058111d1cb6214faf328ad4f
Michael Harris <mwharris7@gmail.com>
2020-05-29 08:17:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,22 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a}
+  return false
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort
+end
